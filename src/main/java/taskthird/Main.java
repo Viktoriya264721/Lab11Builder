3dea17a7a0836aa2121b483affc85837ca2a7471
Viktoriya264721 <stetsyshyn.pn@ucu.edu.ua>
2023-12-26 20:14:21 +0200

    6

--- src/main/java/taskthird/Main.java
+++ src/main/java/taskthird/Main.java
@@ -2,7 +2,6 @@ package taskthird;
 
 public class Main {
     public static void main(String[] args) {
-        MyImage img = 
-        new RealImage("D:\\Studying\\OOP\\lab11\\Lab11Builder\\src\\main\\java\\taskthird\\dog.jpg");
+        MyImage img = new RealImage("taskthird\\dog.jpg");
     }
 }

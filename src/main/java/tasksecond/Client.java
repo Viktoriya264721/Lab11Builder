82a0503d30867ebfd5b23fba93d4c4290f6d819b
Viktoriya264721 <stetsyshyn.pn@ucu.edu.ua>
2023-12-26 20:26:20 +0200

    8

--- src/main/java/tasksecond/Client.java
+++ src/main/java/tasksecond/Client.java
@@ -3,24 +3,22 @@ package tasksecond;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 public class Client {
-    private int id;
     private static int counter = 0;
+    private int id;
     private String name;
     private String email;
     private int age;
     private Gender gender;
-    
 
     public Client(String name, int age, String email, Gender gender) {
         this.name = name;
         this.age = age;
         this.email = email;
         this.gender = gender;
-        id = counter++;
+        this.id = counter++;
     }
 
     public enum Gender {
--- src/main/java/tasksecond/User.java
+++ src/main/java/tasksecond/User.java
@@ -1,9 +1,5 @@
 package tasksecond;
 
-enum Gender {
-    MALE, FEMALE
-}
-
 public class User {
     private String name;
     private int age;
@@ -11,36 +7,16 @@ public class User {
     private double weight;
     private double height;
 
-    public User(String name, int age, Gender gender,
-    double weight, double height) {
+    public User(String name, int age, Gender gender, double weight, double height) {
         this.name = name;
-        this.gender = gender;
         this.age = age;
+        this.gender = gender;
         this.weight = weight;
         this.height = height;
     }
 
-    // public User(String name, int age, Gender gender) {
-    //     this.name = name;
-    //     this.gender = gender;
-    //     this.age = age;
-    //     this.weight = 0;
-    //     this.height = 0;
-    // }
+    public enum Gender {
+        MALE, FEMALE
+    }
 
-    // public User(String name, Gender gender) {
-    //     this.name = name;
-    //     this.gender = gender;
-    //     this.age = 0;
-    //     this.weight = 0;
-    //     this.height = 0;
-    // }
-
-    // public User(String name) {
-    //     this.name = name;
-    //     this.gender = null;
-    //     this.age = 0;
-    //     this.weight = 0;
-    //     this.height = 0;
-    // }
 }

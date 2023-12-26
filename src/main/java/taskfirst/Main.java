fc0f32dc2fe05912ee2ebcb83a9beda11e0f1d41
Viktoriya264721 <stetsyshyn.pn@ucu.edu.ua>
2023-12-26 20:02:00 +0200

    4

--- src/main/java/taskfirst/Main.java
+++ src/main/java/taskfirst/Main.java
@@ -2,15 +2,19 @@ package taskfirst;
 
 import java.util.Arrays;
 
+import java.util.Arrays;
+
 public class Main {
+    private static final int STUDENT_AGE = 19;
+
     public static void main(String[] args) {
         Student user = Student.builder()
                 .name("Viktoriya")
-                .age(19)
+                .age(STUDENT_AGE)
                 .gender("f")
-                .mark(97)
-                .mark(67)
-                .grades(Arrays.asList(61, 62))
+                // .mark(97)
+                // .mark(67)
+                // .grades(Arrays.asList(61, 62))
                 .build();
         System.out.println(user);
     }
